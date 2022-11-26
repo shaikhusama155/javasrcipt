58b05de787e7f54871453191d3754df35d1ef944
shaikhusama155 <shaikhusama155@gmail.com>
2022-11-26 14:46:56 +0500

    some new

---
+++ for_loop.js
@@ -0,0 +1,31 @@
+var matchFound = "no";
+for (var i = 0; i <= 4; i++);{
+ if (cityToCheck === cleanestCities[i]){
+     matchFound = "yes";
+     alert("It's one of the cleanest cities");
+ }
+}
+if (matchFound === "no"){
+    alert("It's not on the list");
+}
+ var matchFound = false;
+ for (var i = 0; i <= 4; i++);{
+    if (cityToCheck === cleanestCities[i]) {
+       matchFound = true;
+       alert("It's one of the cleanest cities");
+       break;
+     }
+ } 
+
+ if (matchFound === false){
+    alert("It's not on the list");
+ }
+ var numElements = cleanestCities.length;
+ var matchFound = false;
+ for (var i = 0; i < numElements; i++);{
+    if (cityToCheck === cleanestCities[i]) {
+        matchFound = true;
+        alert("It's one of the cleanest cities");
+               break;
+    }
+ }
---
+++ string.js
@@ -0,0 +1,24 @@
+let name = "Usama jawaid"
+console.log(name);
+
+// template litrals
+let boy1 = `Usama`
+let boy2 = `khizar`
+// usama is a freind of khizar
+let sentence = `${boy1} is a freind of ${boy2}`
+console.log(sentence)
+
+
+// Escape sequence Characters 
+let newname = 'brother\''
+console.log(newname)
+
+console.log(name.toUpperCase)
+console.log(name.toLowerCase)
+console.log(name.slice(2,4))
+console.log(name.replace("Usama","Aliyan"))
+
+
+// some new discuss
+let freind = `Rahim`
+console.log(name.contact(" is a freind of ", freind))
